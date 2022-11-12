2c397f92e6584976156d4aae26b2151d92d75742
Emilio Oberto <skodiak77@gmail.com>
2022-11-12 18:13:12 -0500

    Created data for transactions and admins handling

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -3,4 +3,26 @@ pragma solidity ^0.8.0;
 
 contract Escrow {
     
+    address payable public owner;
+
+    enum Status { PENDING, CONFIRMED, DISPUTED, REFUNDED, RELEASED }
+
+    struct Transaction {
+        
+        address sender;
+        address receiver;
+        uint256 amount;
+        Status status;
+
+    }
+
+    mapping(uint256 => Transaction) public transactions;
+
+    mapping(address => bool) public admins;
+
+    constructor() {
+
+        owner = payable(msg.sender);
+
+    }
 }

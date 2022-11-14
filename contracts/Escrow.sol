e2a93078fdd17ef70b2c2c15da7c2a150cef2d4e
Emilio Oberto <skodiak77@gmail.com>
2022-11-14 02:30:37 -0500

    Added withdrew status, and added deposit and confirmation functions

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -5,7 +5,9 @@ contract Escrow {
     
     address payable public owner;
 
-    enum Status { PENDING, CONFIRMED, DISPUTED, REFUNDED, RELEASED }
+    uint256 public txid = 1;
+
+    enum Status { PENDING, CONFIRMED, WITHDREW, DISPUTED, REFUNDED, RELEASED }
 
     struct Transaction {
         
@@ -25,4 +27,33 @@ contract Escrow {
         owner = payable(msg.sender);
 
     }
+
+    function Deposit(address _receiver) external payable {
+
+        require(_receiver != address(0), "Cannot escrow funds to zero address");
+
+        transactions[txid] = Transaction(
+
+            msg.sender,
+            _receiver,
+            msg.value,
+            Status.PENDING
+
+        );
+
+        txid ++;
+
+        //ADD EVENT FOR ESCROW CREATION
+    }
+
+    function Confirm(uint256 _id) external {
+
+        //TRANSFORM INTO A MODIFIER
+        require(msg.sender == transactions[_id].sender, "Transactions can only be confirmed by the sender");
+
+        transactions[_id].status = Status.CONFIRMED;
+
+        //ADD EVENT FOR SENDER CONFIRMATION
+    }
+
 }
